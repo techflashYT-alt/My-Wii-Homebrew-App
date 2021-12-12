f64715a5538c07d63c762858c0c3040a1056838a
sanikdah <michaelfgarofalo@gmail.com>
2021-12-12 00:49:11 -0700

    Add the new ScrollAway and ChangeState functions

--- source/head/funcDecl.h
+++ source/head/funcDecl.h
@@ -7,7 +7,8 @@ void StartClose();
 char* checkAspect();
 void getControllerInput();
 void ClearConsole();
-
+void ScrollAway();
+void ChangeState(char state[25]);
 
 //void initWiiJunk();
 // void displayMenu(char* menuName[100], bool Horizontal, int optionsNum, struct MenuOptions MenuOptions);
