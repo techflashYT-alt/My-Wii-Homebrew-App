c7ff5efd68aa86028092769fb2cbbaca912b6599
sanikdah <michaelfgarofalo@gmail.com>
2021-12-13 00:48:24 -0700

    Add a placeholder help screen.

---
+++ source/HelpScreen.c
@@ -0,0 +1,9 @@
+#include "head/funcDecl.h"
+#include "head/StdC_Headers.h"
+
+void HelpScreen() {
+    ScrollScreen();
+    ClearScreen();
+    printf("Sorry!  This is nothing but a placeholder for now.  I\x0A");
+    printf("will add real help here soon, maybe even ONLINE UPDATING help ;)");
+}
