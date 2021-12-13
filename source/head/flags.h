7edeffd5067adc4de5a3365749e95cb2d6094b2f
sanikdah <michaelfgarofalo@gmail.com>
2021-12-13 00:53:15 -0700

    Various additions

--- source/head/flags.h
+++ source/head/flags.h
@@ -2,9 +2,20 @@
 
 #include <stdbool.h>
 
+
+
+
 #ifndef debugMode
 #define debugMode true
 #endif
+
+#ifndef ESC
+#define ESC "\033"
+#endif
+
+extern const char *CurrentState;
+extern const char *state;
+
 
 // #ifndef CurrentState
 // #define CurrentState
