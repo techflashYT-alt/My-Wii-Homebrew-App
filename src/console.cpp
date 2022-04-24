c5b67ce64760aa5d1452eac6509972d434a9b5df
techflashYT <michaelfgarofalo@gmail.com>
2022-04-24 12:06:38 -0700

    Comments

--- src/console.cpp
+++ src/console.cpp
@@ -1,13 +1,32 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
+/*!
+	\fn
+	\brief Clear the console screen.
+
+	\returns Nothing
+*/
 void consoleClear() {
-	sleep(1);
 	printf("\e[2;0H\e[2J");
 }
+/*!
+	\fn
+	\brief Moves the cursor the specified row and column.
+	
+	\param x The row to move the cursor to.
+	\param y The column to move the cursor to.
+	\returns Nothing
+*/
 void consoleMove(uint8_t x, uint8_t y) {
 	printf("\e[%d;%dH", y, x);
 }
+/*!
+	\fn
+	\brief Scroll the console up by printing a ton of newlines.
+	
+	\returns Noting
+*/
 void consoleScroll() {
 	printf("\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n");
 	consoleClear();
